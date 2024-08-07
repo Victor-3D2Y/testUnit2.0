0d0142b7624f162f867dbf320cf2b63cea670dc0
Victor Hugo <victorhugosimidao@gmail.com>
2024-08-07 20:49:10 -0300

    Add files via upload

---
+++ tests copy.js
@@ -0,0 +1,52 @@
+// Validar senha (requerida e deve conter pelo menos um caractere especial)
+
+
+var specialCharacters = /[!@#$%^&*()_+\-={}[\]|:;'"<>,.?/]/;
+
+function validatePassword(password) {
+    if (password === "") {
+        // displayPasswordMessage("Por favor, informe sua senha.");
+        return false;
+    } else if (!specialCharacters.test(password)) {
+        // displayPasswordMessage("A senha deve conter pelo menos um caractere especial.");
+        return false;
+    } else {
+        return true
+    }
+
+}
+
+// Validar confirmação de senha
+function confirmSenha(password) {
+    if (confirmPassword === "" && password === "") {
+        // displayConfirmPasswordMessage("Por favor, confirme sua senha.");
+        return false;
+    } else if (password !== confirmPassword) {
+        // displayConfirmPasswordMessage("As senhas digitadas não correspondem. Por favor, digite novamente.");
+        return false;
+    } else {
+        return true
+    }
+}
+
+function confirmAge(age) {
+    if (age === "" || parseInt(age) < 18) {
+
+        return false;
+    } else {
+        return true
+    }
+}
+
+function onSubmit(password, confirmPassword, age) {
+    const boleanPassword = validatePassword(password, confirmPassword)
+    const boleanAge = confirmAge(age)
+
+    if (boleanPassword == true && boleanAge == true) {
+        return true
+    } else {
+        return false
+    }
+}
+
+module.exports = { validatePassword, confirmAge, onSubmit }
